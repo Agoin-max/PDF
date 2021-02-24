9a29a133a5733aa953e1a40effab3ec88d10887d
Administrator <254327863@qq.com>
2021-02-24 20:32:12 +0800

    update 2021-02-24 20:32

--- Unclassified_AiRui.py
+++
@@ -1,8 +0,0 @@
-from AirRui import AirModel
-
-
-class UnAirModel(AirModel):
-
-    def time_strappend(self, file):
-        path_list = file.rsplit("-", 2)
-        return path_list[0] + "-" + self.time_strftime() + ".xlsx"
